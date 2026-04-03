7d08664673ebe04a7cd3c71a5cc472eba4f9d290
ahdg6 <>
2026-04-03 23:10:02 +0800

    c

--- .tmp/pwui-test-1/assets/hostInit-DTpR4Rer.js
+++
@@ -1,1 +0,0 @@
-import "./pluxel_ext_PluginWithUI_6jdrs0__H_A_I__hostAutoInit__H_A_I__-Cm0qJ30r.js";
--- .tmp/pwui-test-1/remoteEntry.js
+++
@@ -1,3 +0,0 @@
-import "./assets/dist-Bkn8ZjXc.js";
-import { n as init, t as getExposes } from "./assets/virtual_mf-REMOTE_ENTRY_ID_pluxel_ext_PluginWithUI_6jdrs0__remoteEntry_js-BD--uf2H.js";
-export { getExposes as get, init };
